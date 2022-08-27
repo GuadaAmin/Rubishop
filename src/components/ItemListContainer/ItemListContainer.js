ffa68f63089a1e5b210ba1cb58475510fd12fe59
guadaamin <guaddamin@gmail.com>
2022-08-27 14:24:44 -0300

    items

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './ItemListContainer.css';
 import ItemCount from "../ItemCount/ItemCount";
+import ItemList from "../ItemList/ItemList";
 
 const ItemListContainer = ({greeting}) => {
     return (
         <div className="itemListContainer">
             <p>{greeting}</p>
             <ItemCount nombreProducto="Cubo 3×3×3" />
+            <ItemList />
         </div>
     );
 }
